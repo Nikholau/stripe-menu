88fe7ad51fc4de231c5f0b9352ba3ae3d6d9984c
Nikholau <nikholasyan12@gmail.com>
2021-07-12 17:27:05 -0300

    first commit

---
+++ src/components/DropDown/Option.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function DropDownOption({ name, content: Content }) {
+    return (
+        <button>{name}</button>
+    )
+}
---
+++ src/components/DropDown/index.js
@@ -0,0 +1,1 @@
+export { DropDownOption} from './Option'
